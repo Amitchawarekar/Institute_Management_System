0fa081766f7a0dd40d69310d52910f1d1dcabf97
Amit <amit.chawarekar@gmail.com>
2021-10-31 09:29:49 +0530

    added urls for static,media path and for demo also

--- Institute_Management_System/Institute_Management_System/urls.py
+++ Institute_Management_System/Institute_Management_System/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from institute_management_app import views
+from Institute_Management_System import settings
 
 urlpatterns = [
+    path('demo',views.showDemoPage),
     path('admin/', admin.site.urls),
-]
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
