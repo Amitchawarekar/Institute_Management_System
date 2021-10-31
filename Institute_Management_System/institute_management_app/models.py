8d700887dd85ed3f6bc60bd5f9bcafaf58eab85f
Amit <amit.chawarekar@gmail.com>
2021-10-31 08:52:07 +0530

    Added models into models.py

--- Institute_Management_System/institute_management_app/models.py
+++ Institute_Management_System/institute_management_app/models.py
@@ -1,3 +1,113 @@
 from django.db import models
 
 # Create your models here.
+class AdminHOD(models.Model):
+    id = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager() # It is used to return current object data
+
+class Staffs(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager() 
+
+
+
+class Courses(models.Model):
+    id=models.AutoField(primary_key=True)
+    course_name=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Students(models.Model):
+    id = models.AutoField(primary_key=True)
+    batch= models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
+    gender=models.CharField(max_length=255)
+    contact1 =models.IntegerField(max_length=255)
+    contact2 =models.IntegerField(max_length=255)
+    email=models.CharField(max_length=255)
+    dob = models.CharField(max_length=255)
+    course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    course = models.CharField(max_length=255)
+    coursefees = models.CharField(max_length=255)
+    amount_paid = models.IntegerField(max_length=255)
+    amount_paid = models.IntegerField(max_length=255)
+    date_ap = models.DateTimeField(auto_now_add=True)
+    balance = models.IntegerField(max_length=255)
+    password=models.CharField(max_length=255)
+    profile_pic = models.FileField()
+    address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class Attendance(models.Model):
+    id=models.AutoField(primary_key=True)
+    course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    attendance_date=models.DateTimeField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class AttendanceReport(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    attendance_id=models.ForeignKey(Attendance,on_delete=models.CASCADE)
+    status=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+
+
+
+class FeedBackStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class FeedBackStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class NotificationStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class NotificationStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    
+
+
