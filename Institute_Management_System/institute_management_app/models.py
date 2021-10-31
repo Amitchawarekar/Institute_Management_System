1e4e6af2a041d829dab444407567d8a3077ff998
Amit <amit.chawarekar@gmail.com>
2021-10-31 19:29:27 +0530

    Creating CustomUser for admin,staff,student

--- Institute_Management_System/institute_management_app/models.py
+++ Institute_Management_System/institute_management_app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+    user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
+    user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
+
 class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     name=models.CharField(max_length=255)
     email=models.CharField(max_length=255)
     password=models.CharField(max_length=255)
@@ -12,6 +17,7 @@ class AdminHOD(models.Model):
 
 class Staffs(models.Model):
     id=models.AutoField(primary_key=True)
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     name=models.CharField(max_length=255)
     email=models.CharField(max_length=255)
     password=models.CharField(max_length=255)
@@ -31,6 +37,7 @@ class Courses(models.Model):
 
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     batch= models.CharField(max_length=255)
     name=models.CharField(max_length=255)
     gender=models.CharField(max_length=255)
