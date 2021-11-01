d0ac895194caa5c5d0e1c26a75275934d3935311
Amit <amit.chawarekar@gmail.com>
2021-11-01 11:19:43 +0530

    created view for loginpage and dologin button

--- Institute_Management_System/institute_management_app/views.py
+++ Institute_Management_System/institute_management_app/views.py
@@ -1,4 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def showDemoPage(request):
     return render(request, "demo.html")
+
+def ShowLoginPage(request):
+    return render(request,'login_page.html')
+
+def doLogin(request):
+    if request.method != 'POST':
+        return HttpResponse("<h2>Method is not Allowe</h2>")
+    else:
+        return HttpResponse("Email : " + request.POST.get("email") + "Password : " + request.POST.get("password"))
+        
