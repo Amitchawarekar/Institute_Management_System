53eeb938316f53994ba4d19da0d05a691e2faaaf
Amit <amit.chawarekar@gmail.com>
2021-11-01 11:56:51 +0530

    added login functions for emailbackend.py

--- Institute_Management_System/institute_management_app/views.py
+++ Institute_Management_System/institute_management_app/views.py
@@ -1,5 +1,11 @@
-from django.http.response import HttpResponse
+import datetime
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+
+from institute_management_app.EmailBackEnd import EmailBackEnd
 
 def showDemoPage(request):
     return render(request, "demo.html")
@@ -8,8 +14,24 @@ def ShowLoginPage(request):
     return render(request,'login_page.html')
 
 def doLogin(request):
-    if request.method != 'POST':
-        return HttpResponse("<h2>Method is not Allowe</h2>")
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        return HttpResponse("Email : " + request.POST.get("email") + "Password : " + request.POST.get("password"))
-        
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
+        if user!=None:
+            login(request,user)
+            return HttpResponse("Email :" + request.POST.get("email")+"Password : "+request.POST.get("password"))
+        else:
+            # messages.error(request,"Invalid Login Details")
+            return HttpResponse("Invalid Login")
+
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User : "+request.user.email+" usertype : "+str(request.user.user_type))
+    else:
+        return HttpResponse("Please Login First")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
